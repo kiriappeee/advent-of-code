332d52b05315263a8d450f33f7f0e3b11411d99e
Adnan Issadeen <adnan.issadeen@hey.com>
2020-12-01 06:12:30 +0000

    Day 1 part 2 complete

--- 2020/python/day01.py
+++ 2020/python/day01.py
@@ -1,17 +1,27 @@
 import sys
 
-def find_numbers_that_sum_up_to_expected(numbers=[], expected_sum=0):
+def find_two_numbers_that_sum_up_to_expected(numbers=[], expected_sum=0):
   for i in range(0, len(numbers)):
     for j in range(i+1, len(numbers)):
       if(sum([numbers[i], numbers[j]])) == 2020:
         return [numbers[i], numbers[j]]
+
+def find_three_numbers_that_sum_up_to_expected(numbers=[], expected_sum=0):
+  for i in range(0, len(numbers)):
+    for j in range(i+1, len(numbers)):
+      for k in range(j+1, len(numbers)):
+        if(sum([numbers[i], numbers[j], numbers[k]])) == 2020:
+          return [numbers[i], numbers[j], numbers[k]]
 
 if __name__ == "__main__":
   args = sys.argv
   with open('python/inputday01.txt') as f:
     puzzle_input = [int(x) for x in f.readlines() if x.strip() != ""]
   if args[1] == "1":
-    print(puzzle_input)
-    output = find_numbers_that_sum_up_to_expected(numbers=puzzle_input, expected_sum=2020)
+    output = find_two_numbers_that_sum_up_to_expected(numbers=puzzle_input, expected_sum=2020)
     print(output)
     print(output[0]*output[1])
+  if args[1] == "2":
+    output = find_three_numbers_that_sum_up_to_expected(numbers=puzzle_input, expected_sum=2020)
+    print(output)
+    print(output[0]*output[1]*output[2])
--- 2020/python/test-day01.py
+++ 2020/python/test-day01.py
@@ -6,8 +6,12 @@ class TestDay1(unittest.TestCase):
     self.assertEqual('hello', 'hello')
   def test_can_find_two_integers_that_add_up_to_2020(self):
     test_list = [1721,979,366,299,675,1456]
-    answer = day01.find_numbers_that_sum_up_to_expected(numbers=test_list, expected_sum=2020)
+    answer = day01.find_two_numbers_that_sum_up_to_expected(numbers=test_list, expected_sum=2020)
     self.assertEqual(sum(answer), 2020)
-    
+  def test_can_find_three_integers_that_add_up_to_2020(self):
+    test_list = [1721,979,366,299,675,1456]
+    answer = day01.find_three_numbers_that_sum_up_to_expected(numbers=test_list, expected_sum=2020)
+    self.assertEqual(sum(answer), 2020)
+  
 if __name__ == "__main__":
   unittest.main()
