ee77bcb59e8874612f6eb34c54f989ee9d1123a0
Adnan Issadeen <adnan.issadeen@hey.com>
2020-12-02 11:55:18 +0000

    Completed day 2 part 2

--- 2020/python/day02.py
+++ 2020/python/day02.py
@@ -10,9 +10,21 @@ def break_input_into_rules(password_line_input):
   output['password'] = password_line_input.split(':')[1].strip()
   return output
 
+def break_input_into_new_rules(password_line_input):
+  output = {}
+  rules = password_line_input.split(':')[0]
+  output['first_position'] = int(rules.split('-')[0]) - 1
+  output['second_position'] = int(rules.split('-')[1].split(' ')[0]) - 1
+  output['test_letter'] = rules.split('-')[1].split(' ')[1]
+  output['password'] = password_line_input.split(':')[1].strip()
+  return output
+
 def validate_password(password_with_rules):
   c = Counter(password_with_rules['password'])
   return c[password_with_rules['test_letter']] >= password_with_rules['min_times'] and c[password_with_rules['test_letter']] <= password_with_rules['max_times']
+
+def validate_password_with_new_rules(password_with_rules):
+  return password_with_rules['password'][password_with_rules['first_position']] == password_with_rules['test_letter'] and password_with_rules['password'][password_with_rules['second_position']] != password_with_rules['test_letter'] or password_with_rules['password'][password_with_rules['first_position']] != password_with_rules['test_letter'] and password_with_rules['password'][password_with_rules['second_position']] == password_with_rules['test_letter']
 
 if __name__ == "__main__":
   args = sys.argv
@@ -24,5 +36,12 @@ if __name__ == "__main__":
     for pi in puzzle_input:
       rule_set = break_input_into_rules(pi)
       if validate_password(rule_set):
+        counter+=1
+    print(counter)
+  if args[1] == "2":
+    counter = 0
+    for pi in puzzle_input:
+      rule_set = break_input_into_new_rules(pi)
+      if validate_password_with_new_rules(rule_set):
         counter+=1
     print(counter)
--- 2020/python/test-day02.py
+++ 2020/python/test-day02.py
@@ -1,7 +1,7 @@
 import unittest
 from . import day02
 
-class TestDay1(unittest.TestCase):
+class TestDay2(unittest.TestCase):
   def test_input_can_be_broken_correctly(self):
     test_input = '1-3 a: abcde'
     output = day02.break_input_into_rules(test_input)
@@ -12,6 +12,7 @@ class TestDay1(unittest.TestCase):
       'password': 'abcde'
     }
     self.assertEqual(output, expected_output)
+
   def test_password_can_be_validated(self):
     test_rule = {
       'min_times': 1,
@@ -34,6 +35,40 @@ class TestDay1(unittest.TestCase):
       'password': 'ccccccccc'
     }
     self.assertTrue(day02.validate_password(test_rule))
+
+  def test_input_can_be_broken_down_based_on_second_rules(self):
+    test_input = '1-3 a: abcde'
+    output = day02.break_input_into_new_rules(test_input)
+    expected_output = {
+      'first_position': 0,
+      'second_position': 2,
+      'test_letter': 'a',
+      'password': 'abcde'
+    }
+    self.assertEqual(output, expected_output)
   
+  def test_password_can_be_validated(self):
+    test_rule = {
+      'first_position': 0,
+      'second_position': 2,
+      'test_letter': 'a',
+      'password': 'abcde'
+    }
+    self.assertTrue(day02.validate_password_with_new_rules(test_rule))
+    test_rule = {
+      'first_position': 0,
+      'second_position': 2,
+      'test_letter': 'b',
+      'password': 'cdefg'
+    }
+    self.assertFalse(day02.validate_password_with_new_rules(test_rule))
+    test_rule = {
+      'first_position': 1,
+      'second_position': 8,
+      'test_letter': 'c',
+      'password': 'ccccccccc'
+    }
+    self.assertFalse(day02.validate_password_with_new_rules(test_rule))
+
 if __name__ == "__main__":
   unittest.main()
