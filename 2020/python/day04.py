85cce1425ce08014a363a7d087cd75d5173fb927
Adnan Issadeen <adnan.issadeen@hey.com>
2020-12-04 10:23:19 +0000

    Completed day 4 part 2

--- 2020/python/day04.py
+++ 2020/python/day04.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 valid_fields = [
   "byr",
@@ -34,21 +35,88 @@ def passport_is_valid(passport_string_to_test):
       return False
   return True
 
+def validate_birth_year(birth_year_string):
+  birth_year = int(birth_year_string.split(':')[1])
+  return birth_year >= 1920 and birth_year <= 2002 and len(birth_year_string.split(":")[1]) == 4
+
+def validate_issue_year(issue_year_string):
+  issue_year = int(issue_year_string.split(':')[1])
+  return issue_year >= 2010 and issue_year <= 2020 and len(issue_year_string.split(":")[1]) == 4
+
+def validate_expiry_year(expiry_year_string):
+  expiry_year = int(expiry_year_string.split(':')[1])
+  return expiry_year >= 2020 and expiry_year <= 2030 and len(expiry_year_string.split(":")[1]) == 4
+
+def validate_height(height_string):
+  height_string = height_string.replace('hgt:', '')
+  if re.search('^\d{3}cm$|\d{2}in$', height_string):
+    if height_string.endswith('cm'):
+      height = int(height_string[:3])
+      return height >=150 and height <= 193
+    else:
+      height = int(height_string[:2])
+      return height >= 59 and height <= 76
+  else:
+    return False
+
+def validate_hair_colour(hair_colour_string):
+  hair_colour = hair_colour_string.replace("hcl:", "")
+  return re.search('^#[0-9a-f]{6}$', hair_colour) is not None
+
+def validate_eye_colour(eye_colour_string):
+  eye_colour = eye_colour_string.replace("ecl:", "")
+  return re.search('^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$', eye_colour) is not None
+
+def validate_passport_id(passport_id_string):
+  passport_id = passport_id_string.replace("pid:", "")
+  return re.search('^[0-9]{9}$', passport_id) is not None
+
+def passport_is_valid_with_new_rules(passport_string_to_test):
+  if not passport_is_valid(passport_string_to_test):
+    return False
+  
+  passport_variables = passport_string_to_test.split(" ")
+  for pv in passport_variables:
+    
+    if pv.startswith('byr') and not validate_birth_year(pv):
+      print(f'Invalidated byr {pv} for {passport_string_to_test}')
+      return False
+    elif pv.startswith('iyr') and not validate_issue_year(pv):
+      print(f'Invalidated iyr {pv} for {passport_string_to_test}')
+      return False
+    elif pv.startswith('eyr') and not validate_expiry_year(pv):
+      print(f'Invalidated eyr {pv} for {passport_string_to_test}')
+      return False
+    elif pv.startswith('hgt') and not validate_height(pv):
+      print(f'Invalidated hgt {pv} for {passport_string_to_test}')
+      return False
+    elif pv.startswith('ecl') and not validate_eye_colour(pv):
+      print(f'Invalidated ecl {pv} for {passport_string_to_test}')
+      return False
+    elif pv.startswith('hcl') and not validate_hair_colour(pv):
+      print(f'Invalidated hcl {pv} for {passport_string_to_test}')
+      return False
+    elif pv.startswith('pid') and not validate_passport_id(pv):
+      print(f'Invalidated pid {pv} for {passport_string_to_test}')
+      return False
+  return True
+    
+
+
 if __name__ == "__main__":
   args = sys.argv
   with open('python/inputday04.txt') as f:
     passport_batch_string = f.read()
+  
+  count = 0
+  passport_list = break_input_into_individual_passports(passport_batch_string)
   if args[1] == "1":
-    count = 0
-    passport_list = break_input_into_individual_passports(passport_batch_string)
-    print(passport_list)
-    print(len(passport_list))
-    
     for passport_string in passport_list:
       if passport_is_valid(passport_string):
         count+=1
-      
     print(count)
-
-  if args[1] == "2":
-    pass
+  elif args[1] == "2":
+    for passport_string in passport_list:
+      if passport_is_valid_with_new_rules(passport_string):
+        count+=1
+    print(count)
--- 2020/python/test-day04.py
+++ 2020/python/test-day04.py
@@ -34,6 +34,88 @@ iyr:2011 ecl:brn hgt:59in"""
     self.assertTrue(day04.passport_is_valid(test_passport))
     self.assertFalse(day04.passport_is_valid("hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"))
 
+  def test_birth_year_is_validated_correctly(self):
+    self.assertTrue(day04.validate_birth_year("byr:2002"))
+    self.assertTrue(day04.validate_birth_year("byr:1920"))
+    self.assertTrue(day04.validate_birth_year("byr:1990"))
+    self.assertTrue(day04.validate_birth_year("byr:2000"))
+    self.assertFalse(day04.validate_birth_year("byr:2003"))
+    self.assertFalse(day04.validate_birth_year("byr:2010"))
+    self.assertFalse(day04.validate_birth_year("byr:1919"))
+    self.assertFalse(day04.validate_birth_year("byr:1910"))
+    self.assertFalse(day04.validate_birth_year("byr:20"))
+
+  def test_issue_year_is_validated_correctly(self):
+    self.assertTrue(day04.validate_issue_year("iyr:2010"))
+    self.assertTrue(day04.validate_issue_year("iyr:2020"))
+    self.assertTrue(day04.validate_issue_year("iyr:2015"))
+    self.assertFalse(day04.validate_issue_year("iyr:2009"))
+    self.assertFalse(day04.validate_issue_year("iyr:2021"))
+    self.assertFalse(day04.validate_issue_year("iyr:2001"))
+    self.assertFalse(day04.validate_issue_year("iyr:2001"))
+
+  def test_expiry_year_is_validated_correctly(self):
+    self.assertTrue(day04.validate_expiry_year("eyr:2020"))
+    self.assertTrue(day04.validate_expiry_year("eyr:2030"))
+    self.assertTrue(day04.validate_expiry_year("eyr:2025"))
+    self.assertFalse(day04.validate_expiry_year("eyr:2019"))
+    self.assertFalse(day04.validate_expiry_year("eyr:2036"))
+    self.assertFalse(day04.validate_expiry_year("eyr:2010"))
+
+  def test_height_is_validated_correctly(self):
+    self.assertTrue(day04.validate_height("hgt:150cm"))
+    self.assertTrue(day04.validate_height("hgt:193cm"))
+    self.assertTrue(day04.validate_height("hgt:165cm"))
+    self.assertTrue(day04.validate_height("hgt:59in"))
+    self.assertTrue(day04.validate_height("hgt:76in"))
+    self.assertTrue(day04.validate_height("hgt:65in"))
+    self.assertFalse(day04.validate_height("hgt:200cm"))
+    self.assertFalse(day04.validate_height("hgt:140cm"))
+    self.assertFalse(day04.validate_height("hgt:45in"))
+    self.assertFalse(day04.validate_height("hgt:80in"))
+    self.assertFalse(day04.validate_height("hgt:in"))
+    self.assertFalse(day04.validate_height("hgt:60"))
+
+  def test_hair_colour_is_validated_correctly(self):
+    self.assertTrue(day04.validate_hair_colour("hcl:#602927"))
+    self.assertTrue(day04.validate_hair_colour("hcl:#602abc"))
+    self.assertFalse(day04.validate_hair_colour("hcl:602abc"))
+    self.assertFalse(day04.validate_hair_colour("hcl:#602ab"))
+    self.assertFalse(day04.validate_hair_colour("hcl:#602abg"))
+    self.assertFalse(day04.validate_hair_colour("hcl:dab227"))
+
+  def test_eye_colour_is_validated_correctly(self):
+    for ec in "amb blu brn gry grn hzl oth".split(" "):
+      self.assertTrue(day04.validate_eye_colour(f"ecl:{ec}"))
+    self.assertFalse(day04.validate_eye_colour("ecl:pnk"))
+
+  def test_passport_id_is_validated_correctly(self):
+    self.assertTrue(day04.validate_passport_id("pid:012533040"))
+    self.assertFalse(day04.validate_passport_id("pid:3556412378"))
+
+  def test_invalid_passports_can_be_detected_with_more_rules(self):
+    invalid_passports = [
+      "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+      "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946",
+      "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+      "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007"
+    ]
+
+    for ip in invalid_passports:
+      print(ip)
+      self.assertFalse(day04.passport_is_valid_with_new_rules(ip))
+
+  def test_valid_passports_can_be_detected_with_more_rules(self):
+    valid_passports = [
+      "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f",
+      "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+      "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022",
+      "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+    ]
+
+    for vp in valid_passports:
+      print(vp)
+      self.assertTrue(day04.passport_is_valid_with_new_rules(vp))
 
 if __name__ == "__main__":
   unittest.main()
