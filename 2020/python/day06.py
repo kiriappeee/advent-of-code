a05e743adbf5835ac620a0104979da6e52319538
Adnan Issadeen <eddyrox1@hotmail.com>
2020-12-06 17:54:19 +0530

    reworked input reading as Prep for part 2

--- 2020/python/day06.py
+++ 2020/python/day06.py
@@ -5,19 +5,20 @@ if __name__ == "__main__":
   answer_groups = []
   with open('python/inputday06.txt') as f:
     puzzle_input = [x.strip() for x in f.readlines()]
-    current_group = ''
+    current_group = []
     for l in puzzle_input:
-      current_group += l
       if l == '':
         answer_groups.append(current_group)
-        current_group = ''
-    if current_group != '':
+        current_group = []
+      else:
+        current_group.append(l)
+    if current_group != []:
       answer_groups.append(current_group)
   if args[1] == "1":
     total_yes_count = 0
     for answer_group in answer_groups:
-      print(set(answer_group))
-      total_yes_count += len(set(answer_group))
+      print(set(''.join(answer_group)))
+      total_yes_count += len(set(''.join(answer_group)))
     print(total_yes_count)
   if args[1] == "2":
     pass
